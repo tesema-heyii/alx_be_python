5471c3afcce6175a8b6c74ffffa75166cb235bb0
Tesmea Heyii <tesema.heyii21@gmail.com>
2025-09-13 15:08:43 +0300

    this is the second file added to the first one

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,5 @@
 principal = 1000
 rate = 0.05
 time = 3
-simple_interest = (principal * rate * time)
-print("The simple interest is:", simple_interest)
+interest = (principal * rate * time)
+print("The simple interest is:", interest)
