b84a52d478d8af9859635c44d70ba0d9a0d9d9d5
Tesmea Heyii <tesema.heyii21@gmail.com>
2025-09-13 14:43:53 +0300

    this is basic arthimatic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10 
+number2 = 5
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+quotient = number1 / number2
+print("sum of", number1, "and", number2, "is:", sum)
+print("difference of", number1, "and", number2, "is:", difference)
+print("product of", number1, "and", number2, "is:", product)
+print("quotient of", number1, "and", number2, "is:", quotient)
